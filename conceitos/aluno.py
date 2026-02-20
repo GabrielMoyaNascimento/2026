7bb3beca2f7a56cecd92f1652116a6871b957a29
Gabriel Moya <gabrielmoya201415@gmail.com>
2026-02-20 11:02:31 -0300

    Aula do dia 20/02/26

---
+++ conceitos/aluno.py
@@ -0,0 +1,9 @@
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite sua idade: "))
+telefone = int(input("Digite seu telefone: "))
+cidade = input("Digite a sua cidade: ")
+
+print("Olá ", nome)
+print("Sua idade é ", idade)
+print("Seu telefone é ", telefone)
+print("Você mora em ", cidade)
---
+++ conceitos/matematica.py
@@ -0,0 +1,12 @@
+numero1 = input("Digite o primeiro número: ")
+numero2 = input("Digite o segundo número: ")
+
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+divisao = numero1 / numero2
+multiplicacao = numero1 * numero2
+
+print("A soma é: ", soma)
+print("A subtração é: ", subtracao)
+print("A divisão é:", divisao)
+print("A multiplicação é: ", multiplicacao)
